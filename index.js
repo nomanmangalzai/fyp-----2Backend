24167bb212fa7bc2c95e967ca70d430d4b602cfb
Noman Mangalzai <noman.mangalzai4@gmail.com>
2023-04-10 09:54:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
+//123 testing
 
 require("./src/db/connection");
 app.use(bodyParser.json());
