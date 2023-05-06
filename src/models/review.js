691514250dd27a92e7f531081811efdefbce9448
nomanmangalzai <62675080+nomanmangalzai@users.noreply.github.com>
2023-05-06 14:19:35 +0500

    review schema updated

--- src/models/review.js
+++ src/models/review.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = mongoose.Schema({
-  reviewId: {
-    type: Number,
-  },
-  productName: {
-    type: String,
-    required: true,
-  },
   customerName: {
     type: String,
-    required: true,
+    required: [true, "Please enter the name of the reviewer"],
     //I want to take it from user schema/model
   },
-  Rating: {
+  email: {
     type: String,
-    required: true,
+    required: [true, "Please enter the email of the reviewer"],
+  },
+  Rating: {
+    type: Number, //number
+    required: [true, "Please enter the review rating"],
+  },
+  title: {
+    type: String,
+    required: [true, "Please enter the review title"],
+  },
+  message: {
+    type: String,
+    required: [true, "Please enter the review description"],
   },
   Date: {
     type: Date,
     default: Date.now,
-    required: true,
+    required: [true, "Please enter the date of the review"],
   },
 });
 module.exports = Reviews = mongoose.model("Review", reviewSchema);
