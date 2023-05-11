0efe2d96651bccaba3243f4fc1f50390a4afe1eb
nomanmangalzai <62675080+nomanmangalzai@users.noreply.github.com>
2023-05-11 17:09:34 +0500

    ali anwar admin email added

--- src/controller/isAuthorized.js
+++ src/controller/isAuthorized.js
@@ -1,7 +1,8 @@
 const users = require("../models/auth");
 
 const isAuthorized = (req, res, next) => {
-  const acceptedEmail = "noman.mangalzai4@gmail.com";
+  const acceptedEmai1 = "noman.mangalzai4@gmail.com";
+  const acceptedEmai2 = "alianwar@gmail.com";
 
   users.findById(req.userId, { password: 0 }, function (err, user) {
     if (err) return res.status(500).send("There was a problem.");
@@ -9,7 +10,7 @@ const isAuthorized = (req, res, next) => {
       return res
         .status(404)
         .send("You must have an account to make this request.");
-    if (user.email !== acceptedEmail)
+    if (user.email !== acceptedEmail1 || user.email !== acceptedEmai2)
       return res.status(401).send("You are not authorized.");
     req.email = user.email;
     next();
